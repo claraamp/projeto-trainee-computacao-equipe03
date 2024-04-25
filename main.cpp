50b0658999bc576e6e998dd8ec2eb84f4c9206a5
claraamp <108371255+claraamp@users.noreply.github.com>
2024-04-25 16:36:16 -0300

    inicio do gol (desenho e posicionamento)

--- main.cpp
+++ main.cpp
@@ -20,6 +20,13 @@ int main(int argc, char *argv[]) {
         {
             screenSurface = SDL_GetWindowSurface(window);
             SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 79, 121, 66));
+            SDL_Rect goalRect;
+            goalRect.x = 5;
+            goalRect.y = 240;
+            goalRect.w = 25;  
+            goalRect.h = 100;
+            SDL_FillRect(screenSurface, &goalRect, SDL_MapRGB(screenSurface->format, 255, 255, 255));
+
             SDL_UpdateWindowSurface(window);
             SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
         }
